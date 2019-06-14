b286478733331ee9da781d28720def2fcd675f16
jinxin113437 <jinxin113437@pwrd.com>
2019-06-15 02:06:43 +0800

    初始提交。

---
+++ Solution/Doodle/CLApp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+
+namespace Doodle
+{
+    /// <summary>
+    /// command line application
+    /// </summary>
+    public static class CLApp
+    {
+        private const string HELP_TEMPLATES = "-?|-h|--help";
+
+        private static CommandLineApplication s_rootCommand;
+
+        public static void Exec(string[] args)
+        {
+            s_rootCommand = new CommandLineApplication();
+            s_rootCommand.HelpOption(HELP_TEMPLATES);
+
+            //s_rootCommand.Option("--logFile")
+        }
+    }
+}
---
+++ Solution/Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Test
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var app = new CommandLineApplication(false);
+            app.HelpOption("-h|--help");
+
+            var gloO1 = app.Option("-go1", "global o1", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine($"gloO1: {gloO1.Value()}");
+                return 0;
+            });
+            
+
+
+
+
+            app.Command("TestCommand", c =>
+            {
+                c.Description = "这是一个测试命令。";
+                c.HelpOption("-h|--help");
+
+                var arg1 = c.Argument("arg1", "固定参数1");
+                var opt1 = c.Option("-o1|--opt1", "可选参数1", CommandOptionType.SingleValue);
+
+                c.OnExecute(() =>
+                {
+                    Console.WriteLine("TestCommand执行！");
+                    Console.WriteLine($"固定参数1: {arg1.Value}");
+                    Console.WriteLine($"可选参数1，HasValue：{opt1.HasValue()}, Value: {opt1.Value()}");
+
+                    return int.Parse(arg1.Value);
+                });
+            }, false);
+
+            return new CommandLineApplication()
+        }
+    }
+}
