29d295e1f72faccf064d6171a78ed12048c03145
harveyjin <china.jinxin@foxmail.com>
2019-06-23 01:19:56 +0800

    Junction Tools

---
+++ Net35Solution/JunctionSrc.bat
@@ -0,0 +1,3 @@
+..\Tools\Junction\junction64.exe C:\Work\Doodle\Net35Solution\Doodle\src C:\Work\Doodle\Solution\Doodle\src
+..\Tools\Junction\junction64.exe C:\Work\Doodle\Net35Solution\Doodle.CommandLineUtils\src C:\Work\Doodle\Solution\Doodle.CommandLineUtils\src
+..\Tools\Junction\junction64.exe C:\Work\Doodle\Net35Solution\Doodle.CLFramework\src C:\Work\Doodle\Solution\Doodle.CLFramework\src
